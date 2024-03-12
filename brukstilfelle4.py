a4a855e8cef53aa1937ee2ef3ec576231bb3c0d8
sigurhag <sigurhag@ntnu.no>
2024-03-12 11:51:01 +0100

    fixed error with date

--- brukstilfelle4.py
+++ brukstilfelle4.py
@@ -6,7 +6,7 @@ con = sqlite3.connect("teater.db")
 
 def finn_forestilling(Dato):
     cursor = con.cursor()
-    cursor.execute("SELECT TS.Navn, COUNT(Billett.BillettID) AS SolgteBilletter FROM Teaterstykke AS TS INNER JOIN Teaterforestilling AS TF on TS.StykkeID = TF.StykkeID LEFT OUTER JOIN Billett ON TF.Dato = Billett.Forestillingsdato WHERE TF.Dato = ? GROUP BY TS.Navn", (Dato, ))
+    cursor.execute("SELECT TS.Navn, COUNT(Billett.BillettID) AS SolgteBilletter FROM Teaterstykke AS TS INNER JOIN Teaterforestilling AS TF on TS.StykkeID = TF.StykkeID LEFT OUTER JOIN Billett ON TF.Dato = Billett.Forestillingsdato AND TF.StykkeID = Billett.StykkeID WHERE TF.Dato = ? GROUP BY TS.Navn", (Dato, ))
     rows = cursor.fetchall()
     print(f"Forestillinger på dato {Dato} er:")
     print(rows)

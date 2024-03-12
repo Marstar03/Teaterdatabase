700d711b6061d803c3bd50197fff39346dc1863c
Markus Klund <markukl@stud.ntnu.no>
2024-03-12 09:37:29 +0100

    Added distinct to brukstilfelle 5 sql

--- brukstilfelle5.py
+++ brukstilfelle5.py
@@ -2,7 +2,7 @@ import sqlite3
 con = sqlite3.connect("teater.db")
 
 cursor = con.cursor()
-cursor.execute('''SELECT Teaterstykke.Navn, Skuespiller.Navn, Rolle.Navn
+cursor.execute('''SELECT distinct Teaterstykke.Navn, Skuespiller.Navn, Rolle.Navn
     FROM (((Skuespiller inner join SpillerRolle on (Skuespiller.AnsattID = SpillerRolle.SkuespillerID))
     inner join Rolle on (SpillerRolle.RolleID = Rolle.RolleID))
     inner join RolleIAkt on (Rolle.RolleID = RolleIAkt.RolleID))
--- brukstilfelle5.sql
+++
@@ -1,5 +0,0 @@
-SELECT Teaterstykke.Navn, Skuespiller.Navn, Rolle.Navn
-FROM (((Skuespiller inner join SpillerRolle on (Skuespiller.AnsattID = SpillerRolle.SkuespillerID))
-inner join Rolle on (SpillerRolle.RolleID = Rolle.RolleID))
-inner join RolleIAkt on (Rolle.RolleID = RolleIAkt.RolleID))
-inner join Teaterstykke on (RolleIAkt.StykkeID = Teaterstykke.StykkeID);
